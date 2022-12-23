2679475b3d8b24fbdf5aa861049a284eca4474c8
Theofilusarifin <theofilusarifin@gmail.com>
2022-12-23 16:53:40 +0700

    completed works

--- src/app/component/completed/completed.component.ts
+++ src/app/component/completed/completed.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
+import { ComicService } from 'src/app/service/comic.service';
 
 @Component({
   selector: 'app-completed',
@@ -6,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./completed.component.scss'],
 })
 export class CompletedComponent implements OnInit {
+  constructor(public cs: ComicService, public ac: AppComponent) {}
 
-  constructor() { }
+  ngOnInit() {
+    this.getComplete();
+  }
 
-  ngOnInit() {}
-
+  // Get Favorite Comic
+  completed_comics = null;
+  async getComplete() {
+    this.cs.getComplete().subscribe((data) => {
+      if (data.result == 'success') {
+        // convert last update into proper string
+        data.comics.forEach(
+          (element: { [x: string]: string | number | Date }) => {
+            element['latest_update'] = this.ac.last_update(
+              element['latest_update']
+            );
+          }
+        );
+        // Assign Data
+        this.completed_comics = data.comics;
+      }
+    });
+  }
 }
