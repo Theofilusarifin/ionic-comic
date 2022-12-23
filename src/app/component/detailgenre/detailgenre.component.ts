ee2b4080737f48901d6da75958d0b030ba0a527d
Theofilusarifin <theofilusarifin@gmail.com>
2022-12-23 18:00:53 +0700

    detail genre done

--- src/app/component/detailgenre/detailgenre.component.ts
+++ src/app/component/detailgenre/detailgenre.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GenreService } from 'src/app/service/genre.service';
+import { ActivatedRoute } from '@angular/router';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-detailgenre',
@@ -6,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detailgenre.component.scss'],
 })
 export class DetailgenreComponent implements OnInit {
+  constructor(
+    public gs: GenreService,
+    public route: ActivatedRoute,
+    public ac: AppComponent
+  ) {}
 
-  constructor() { }
+  ngOnInit() {
+    this.getComic(this.route.snapshot.params['id']);
+  }
 
-  ngOnInit() {}
+  // Get Genres
+  genre_comics = null;
+  genre_name = null;
+  genre_desc = null;
 
+  async getComic(genre_id: number) {
+    this.gs.getById(genre_id).subscribe((data) => {
+      console.log(data);
+      if (data.result == 'success') {
+        // convert last update into proper string
+        data.comics.forEach(
+          (element: { [x: string]: string | number | Date }) => {
+            element['latest_update'] = this.ac.last_update(
+              element['latest_update']
+            );
+          }
+        );
+        // Assign Data
+        this.genre_comics = data.comics;
+        this.genre_name = data.genre.name;
+        this.genre_desc = data.genre.description;
+      }
+    });
+  }
 }
